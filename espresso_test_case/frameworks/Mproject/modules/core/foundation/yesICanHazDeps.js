bd0ae4a0649a480e2dbeab38b38f165952274987
Alexander <a.henka@mwaysolutions.com>
2010-11-17 16:04:35 +0100

    added more test files

---
+++ espresso_test_case/frameworks/Mproject/modules/core/foundation/yesICanHazDeps.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   The M-Project - Mobile HTML5 Application Framework
+// Copyright: ©2010 M-Way Solutions GmbH. All rights reserved.
+// Creator:   alexander
+// Date:      17.11.2010
+// License:   Dual licensed under the MIT or GPL Version 2 licenses.
+//            http://github.com/mwaylabs/The-M-Project/blob/master/MIT-LICENSE
+//            http://github.com/mwaylabs/The-M-Project/blob/master/GPL-LICENSE
+// ==========================================================================
+
+
+m_require('core/foundation/noDepsMan.js');
+m_require('core/foundation/model.js');
+
+var nopes = 'in can haz deps !!';
