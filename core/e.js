8cc6591185b47b67246e21b211564c18a5bb572f
Alexander <a.henka@mwaysolutions.com>
2011-01-31 10:33:17 +0100

    used version: 0.0.8-pre for dev branch

--- core/e.js
+++ core/e.js
@@ -22,7 +22,7 @@ var E;
  * @constructor
  */
 E = exports.E = function() {
-    this.__version__ = '0.0.7';  // Espresso version.
+    this.__version__ = '0.0.8-pre';  // Espresso version.
 };
 
 /**
