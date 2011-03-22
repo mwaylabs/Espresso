6054c52a2b2324094126358b82bf014b6ac9014f
Sven Pfleiderer <sg+github@roothausen.de>
2011-03-22 18:49:14 +0100

    Improve commandline parser

--- core/commands/server.js
+++ core/commands/server.js
@@ -6,29 +6,30 @@
 
 exports.description = 'Development server to compile and run the application for testing business in a webbrowser';
 
+exports.name = 'server';
+
 exports.examples = [
   'server',
   'server --port 8080',
   'server -m --p 2342 --config config.json'
 ];
 
-exports.parameters = {
+exports.options = {
 
   manifest: {
     'description': 'Start the server in manifest mode. Enable generation of cache.manifest',
-    'example': '--manifest',
     'default': false
   },
 
   config: {
     'description': 'Specify a custom config',
-    'example': '--config /path/to/config.json'
+    'hasargument': true
   },
 
   port: {
     'description': 'Specify a custom port',
-    'example': '--port 8080',
-    'default': 8000
+    'default': 8000,
+    'hasargument': true
   }
 
 };
