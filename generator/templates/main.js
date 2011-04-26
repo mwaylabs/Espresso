43146ac105bc06761cac2820b259392e24fdd171
Sven Pfleiderer <sg+github@roothausen.de>
2011-04-26 13:08:25 +0200

    Add new mustache renderer, remove old submodules

---
+++ generator/templates/main.js
@@ -0,0 +1,20 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso {{e_Version}}
+//
+// Project: {{appName}} 
+// ==========================================================================
+
+var {{appName}}  = {{appName}} || {};
+
+{{appName}}.app = M.Application.design({
+
+    entryPage : 'page1', // define the entry/start page of your app. This property must be provided!
+
+    page1: M.PageView.design({
+
+       //Add your code here!
+
+    })
+
+});
---
+++ generator/templates/validator.js
@@ -0,0 +1,38 @@
+// ==========================================================================
+// The M-Project - Mobile HTML5 Application Framework
+// Generated with: Espresso {{e_Version}}
+//
+// Project: {{appName}}
+// Validator: {{name}}
+// ==========================================================================
+
+M.{{name}} = M.Validator.extend({
+
+    /**
+     * The type of this object.
+     *
+     * @type String
+     */
+    type: 'M.{{name}}',
+
+    validate: function(obj) {
+        if (validation_condition) {
+
+          // case not valid
+           this.validationErrors.push({
+                msg: ' is not a valid',
+                modelId: obj.modelId,
+                property: obj.property,
+                viewId: obj.viewId,
+                validator: 'ValidatorName',
+                onSuccess: obj.onSuccess,
+                onError: obj.onError
+           });
+            return NO;
+        } else {
+           // case valid
+             return YES;
+
+        }
+    }
+});
