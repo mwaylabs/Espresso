985a65ca94a79284ae55dbf439f5c1eec453794c
tv <tv+mway@also>
2011-07-20 16:46:28 +0200

    Add SSH deployment module

---
+++ deploy/ssh/index.js
@@ -0,0 +1,39 @@
+/*!
+ * Espresso SSH deployment module.
+ *
+ * Copyright(c) 2011 Panacoda GmbH. All rights reserved.
+ * This file is licensed under the MIT license.
+ */
+
+// Note that this is a wrapper script that spawns the Shell script deploy.sh
+// that does the actual work.
+
+// TODO native node.js SSH deployment
+
+var spawn = require('child_process').spawn;
+
+module.exports = function (name, config) {
+  return function (path, callback) {
+
+    var command = '/bin/sh';
+    var args = [__dirname + '/deploy.sh', path];
+    var options = {
+      cwd: path, // is there any better place?
+      env: Object.create(config)
+    };
+
+    var child = require('child_process').spawn(command, args, options);
+
+    child.stdout.on('data', function (chunk) {
+      process.stderr.write(chunk);
+    });
+
+    child.stderr.on('data', function (chunk) {
+      process.stderr.write(chunk);
+    });
+
+    child.on('exit', function (code) {
+      callback(code !== 0 && new Error('exit code: ' + code));
+    });
+  };
+};
